93ffd9a3cadd086434bd311752054fd726a4c029
Uwem Udoh <slimudoh@yahoo.com>
2019-11-26 14:08:13 +0100

    project setup

---
+++ src/container/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./../assets/css/reset.css";
+import "./../assets/css/app.css";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>The Sales Force Application</h1>
+    </div>
+  );
+}
+
+export default App;
